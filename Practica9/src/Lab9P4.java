9be24f82c97bcaeb60ab497c7ef6778e3db63677
Raúl Varandela <61058725+raulvarandela@users.noreply.github.com>
2020-05-05 13:11:39 +0200

    Add files via upload

---
+++ Practica9/src/Lab9P4.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Lab9P4 {
+    public static void main(String args[]) {
+        ExecutorService es = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        Random r = new Random();
+
+
+        System.out.println("--------------------------------------------------------------------------");
+
+
+        es.execute(new factorial(r.nextInt(15)));
+        es.execute(new factorial(r.nextInt(15)));
+        es.execute(new factorial(r.nextInt(15)));
+        es.execute(new factorial(r.nextInt(15)));
+
+        es.shutdown();
+
+        es.execute(new factorial(r.nextInt(15)));
+    }
+}
+
+class factorial implements Runnable {
+    int numero;
+    int result;
+
+    public factorial(int numero) {
+        this.numero = numero;
+        this.result = 1;
+    }
+
+    @Override
+    public void run() {
+        if ((numero == 0) || (numero == 1)) {
+            result = 1;
+        } else {
+            for (int i = 2; i <= numero; i++) {
+                result *= i;
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println("El numero es: " + numero);
+        System.out.println("Resultado: " + result);
+        System.out.println("--------------------------------------------------------------------------");
+    }
+}
