c32a560239700845ac0ee959f85beafd34b4c8c4
Raúl Varandela <61058725+raulvarandela@users.noreply.github.com>
2020-05-25 17:07:58 +0200

    Add files via upload

--- Practica7/src/LockDemo.java
+++ Practica7/src/LockDemo.java
@@ -7,8 +7,9 @@ public class LockDemo {
         new Thread(new LockThread(lock, sbar, "A")).start();
         new Thread(new LockThread(lock, sbar, "B")).start();
     }
-}// shared object:
+}
 
+// shared object:
 class Shared {
     static int count = 0;
 }
@@ -27,10 +28,10 @@ class LockThread implements Runnable {
     public void run() {
         System.out.println("Start: " + name);
         try {
-            System.out.println("wating:" + name);
+            System.out.println("wating: " + name);
             // lock
             lock.lock();
-            System.out.println("-----locked:" + name);
+            System.out.println("-----locked: " + name);
             shObj.count++;
             System.out.println(shObj.count);
             // sleep:
@@ -39,7 +40,44 @@ class LockThread implements Runnable {
         } catch (InterruptedException e) {
         } finally {
             // unlock
-            System.out.println("-----unlock" + name);
+            System.out.println("-----unlock " + name);
+            lock.unlock();
+        }
+    }
+}
+
+
+//---------------------------------------------------------------------
+class BoundedBuffer {
+    final Lock lock = new ReentrantLock(); //se crea el candado
+    final Condition notFull = lock.newCondition(); //la condición
+    final Condition notEmpty = lock.newCondition(); //otra condición
+    final Object[] items = new Object[100]; //array?
+    int putptr, takeptr, count;
+
+    public void put(Object x) throws InterruptedException {
+        lock.lock(); //se bloquea
+        try {
+            while (count == items.length) notFull.await(); //mientras que el contenido no sea igual al tope, espero
+            items[putptr] = x;
+            if (++putptr == items.length) putptr = 0;
+            ++count;
+            notEmpty.signal();
+        } finally {
+            lock.unlock(); //se desbloquea
+        }
+    }
+
+    public Object take() throws InterruptedException {
+        lock.lock();
+        try {
+            while (count == 0) notEmpty.await(); //mientras que esté vacio, se espera
+            Object x = items[takeptr];
+            if (++takeptr == items.length) takeptr = 0;
+            --count;
+            notFull.signal();//señala
+            return x;
+        } finally {
             lock.unlock();
         }
     }
